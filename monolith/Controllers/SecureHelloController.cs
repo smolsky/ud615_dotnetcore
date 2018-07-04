50b4fb9f142f2a47f097836c09e3ea0859ea8a1b
Evgeny Smolsky <Evgeny.Smolsky@transas.com>
2018-07-05 00:35:04 +0300

    SecureHelloController added (without secure)

---
+++ monolith/Controllers/SecureHelloController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace monolith.Controllers
+{
+
+    [Produces("application/json")]
+    [Route("secure")]
+    public class SecureHelloController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var obj = new {Message = "Hello"};
+            return Json(obj);
+        }
+    }
+}
