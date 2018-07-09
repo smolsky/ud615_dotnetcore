3bd3552432be4658e40a83e7b5cdfb4d3bcb33c8
Evgeny Smolsky <Evgeny.Smolsky@transas.com>
2018-07-09 22:02:22 +0300

    hello service

---
+++ hello/Controllers/SecureHelloController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace monolith.Controllers
+{
+
+    [Produces("application/json")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("secure")]
+    public class SecureHelloController : Controller
+    {
+        [HttpGet]
+        
+        public JsonResult Get()
+        {
+            var obj = new { Message = "Hello (secure)" };
+            return Json(obj);
+        }
+    }
+}
