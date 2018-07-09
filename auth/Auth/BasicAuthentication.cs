baba7ff71bfa7ca60a7bd8205078e7ff717349fa
Evgeny Smolsky <Evgeny.Smolsky@transas.com>
2018-07-09 22:01:48 +0300

    auth service

---
+++ auth/Auth/BasicAuthentication.cs
@@ -0,0 +1,34 @@
+﻿using idunno.Authentication.Basic;
+using monolith.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace monolith.Auth
+{
+    public class BasicAuthentication
+    {
+        public static void Configure(BasicAuthenticationOptions options)
+        {
+            options.Realm = "idunno";
+            options.AllowInsecureProtocol = true;
+            options.Events = new BasicAuthenticationEvents
+            {
+                OnValidateCredentials = context =>
+                {
+                    var identity = UserIdentities.GetIdentity(context.Username, context.Password);
+
+                    if(identity!= null)
+                    {
+                        context.Principal = new ClaimsPrincipal(identity);
+                        context.Success();
+                    }
+
+                    return Task.CompletedTask;
+                }
+            };
+        }
+    }
+}
---
+++ auth/Auth/JwtOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monolith.Auth
+{
+    public class JwtOptions
+    {
+        public const string ISSUER = "MyAuthServer"; // token issuer
+        public const string AUDIENCE = "http://localhost:5000/"; // token consumer 
+        const string KEY = "ud615_dotnetcore_secretkey!123";   // secret key
+        public const int LIFETIME_MINUTES = 10; // 
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
